df45f9601d520d8cd835ed77c54b617e1789481f
Ilyaz <tajibaev200@gmail.com>
2023-05-24 18:49:51 +0600

    first commit

---
+++ homework.py
@@ -0,0 +1,49 @@
+# a = 17391 / 17
+# b = 546 / 17
+# c = 934 / 17
+# if a > b:
+#     print(f"{a} больше {b}")
+# elif c < b:
+#     print(f"{c} меньше {b}")
+# else:
+#     print(f"{c} больше {b}")
+
+
+# 1
+
+# list1 = [3, 5, 2, 1, 4]
+# list2 = [4, 2, 6, 8, 1]
+# merged_list = list(set(list1 + list2))
+# merged_list.sort()
+# print("Объединенный и отсортированный список:", merged_list)
+
+# 2
+# lst = []
+# for i in range(16):
+#     lst.append(i)
+
+# max_lst = max(lst)
+# min_lst = min(lst)
+# print(max_lst + min_lst)
+
+
+# 3
+# month = int(input("Введите номер месяца: "))
+# if month == 12 or month == 1 or month == 2:
+#     print("Сезон года: Зима")
+# elif month >= 3 and month <= 5:
+#     print("Сезон года: Весна")
+# elif month >= 6 and month <= 8:
+#     print("Сезон года: Лето")
+# elif month >= 9 and month <= 11:
+#     print("Сезон года: Осень")
+# else:
+#     print("Некорректный номер месяца")
+
+# a = ["a", "e", "y", "u", "i"]
+# strr = input("Введите слово")
+# new = strr.split()
+
+# for i in new:
+#     if i[0].lower() in a:
+#         print(i)
---
+++ main.py
@@ -0,0 +1,501 @@
+# a = 10
+# b = 3
+# c = 20
+# d = a + b * c
+# print( d ) 
+
+# a  = 10 
+# b = 3 
+# c = a + b 
+# print( c )
+
+# print(7 ** 3)
+
+# a = 23
+# b = 3
+# c = 5
+# d = 2
+# f = 190380
+# j = ((( a + b ) * c )** d) - f
+# print( j )
+
+# a = 6
+# b = 4
+# c = 8
+# print( a > b < c )
+
+# a = 7
+# b = 3
+# c = 4.8
+# d = ( a % b ) * c
+# print( d )
+
+# a = 2.5 + 3.5
+# b = 3 + 3 
+# print( a == b )
+
+# a = 5 
+# b = 6
+# print( a != b )
+
+# print( -21 // 10 )
+
+# a = 2001
+# b = 2023
+# c = 2
+# d = 2
+# f= (( b - a ) + c ) 
+# print( f )
+
+# name = input( "Ведите имия ")
+# print("Здраствуте", name )
+
+# a = int(input(" Ведите первое число:"))
+# b = int(input(" Ведите второе число:"))
+# print("Резултат-", a + b)
+
+# a = 5.5
+# b = 5.5
+# c = 5.5
+# print(( a + b ) * c)
+# a = 20
+# b = 16
+# c = 7.7
+# d = 9
+# e = 11
+# print((a - e ** (b / d)) % c)
+
+# name = "Ilyaz"
+# print(type(name))
+
+# name = input("Ведите имя:")
+# print("Здраствуйте,", name.upper())
+
+# name = input("Ведите имя; ")
+# print("Здраствуйте,", name. title())
+# name = input("Ведите имя; ")
+# print("Здраствуте,", name. lower())
+
+# name = input("Ведите имия: ")
+# age = input("Ведите ваш возрост: ")
+# place = input("Ведите ваше место рождения: ")
+# print(f"Ведите имия - {name} Ведите ваш возраст - {age} Ведите ваше место рождения - {place}")
+
+# name = input("Ведите имия: ")
+# age = input("Ведите ваш возраст: ")
+# file = input("Ваш любимый фильм: ")
+# print(f"Здраствуйте,  {name}, {file} Хороший фильм, ")
+
+# name = 'Ilyaz'
+# lastname = 'Tajibaev'
+# company =  'Apple'
+# fuul= """
+# {0} {1} is a telented \n Python developer.
+# He works \n at {2}.
+# """.format(name, lastname, company)
+# print(fuul)
+# name  = "Ilyaz"
+# lastname = "Tajibaev"
+# print(lastname.endswith("T"))
+# print(lastname.startswith("T"))
+# print(lastname.split(" "))
+# print(lastname.count("a"))
+# print(len(lastname))
+# print(lastname.center(50))
+
+# name = "Ilyaz"
+# lastname ='Tajibaevilyaztayurbtkjvich'
+# print(lastname[0::5])
+# print(lastname[4])
+# print(lastname[0:11])
+
+# place = "Madagaskar"
+# print(place[0:5])
+
+# name = "Ilyaz"
+# lastname = "Tajibaev"
+# print(lastname + name)
+# name = "Ilyaz"
+# lastname = "Tajibaev"
+# print(name * 5)
+# name = "Ilyaz"
+# lastname = input("Ведите фамилю: ")
+# print(f"Здраствуйте {lastname}.{name[0]}")
+
+# num1 = 5
+# num2 = 5
+# if num1 > num2:
+#     print(f"{num1} больше {num2}")
+# elif num1 == num2:
+#     print(f"{num1} равен {num2}")
+# else:
+#     print(f"{num1} меньше {num2}")
+# num = int(input("Ведите число:"))
+# if num == 0:
+#     print(f"{num} нейтральный")
+# elif num % 2 == 0:
+#     print(f"{num} четный")
+#  else:
+#     rint(f"{num} нечетный")
+
+# a = 2 ** 3
+# b = 2 ** 3
+# if a > b:
+#     print(f"{a} больше {b}")
+# elif a == b:
+#     print(f"{a} равен {b}")
+# else:
+#     print(f"{a} меньше {b}")
+
+
+# num = int(input("Ведите число: "))
+# if num > 0 and num <= 21:
+#      print(f"{num} разрешен")
+# elif num == 57 and num <= 100:
+#     print(f"{num} разрешен")
+# else:
+#     print(f"{num} не разрешен")
+
+# num = int(input("Ведите число: "))
+# if num % 2 == 0: 
+#     print(f"{num} четный")
+
+# else:
+#     print(f"{num} нечетный")
+
+# if num % 3 == 0:
+#     print(f"{num} делится на 3 без остатка")
+# else:
+#     print(f"{num} не делится на 3 без остатка")
+
+# if num ** 2 > 1000:
+#     print(f"{num} в квадрате больше 1000")
+# else:
+#     print(f"{num} в квадрате меньше 1000")
+
+# a = 10 // 5
+# b = 10 / 5
+# if a > b:
+#     print(f"{a} больше {b}")
+# elif a == b:
+#     print(f"{a} равен {b}")
+# else:
+#     print(f"{a} меньше {b}")
+
+# print(a + b)
+
+# num = int(input("Ведите число:"))
+# if num > 0:
+#     print(f'{num} Положительное чило')
+# elif num < 0:
+#     print(f"{num} отрициальное число")
+
+# else:
+#     print(num)
+
+# age = int(input())
+
+# if age >= 18:
+#     print("Совершеннолетний")
+# elif age >= 4 and age <= 4:
+#     print("Ребенок")
+# else:
+#     print("Несовершеннолетний")
+
+# num = [1, 2, 2, 5,]
+# num.append(23)
+# num.append("one")
+# num.remove(2)
+# num.remove(23)
+# print(num.count(2))
+# name = ["Ilyaz", "asan"]
+# # num1 = [1, 4, 7, 3, 5,]
+# # num.extend(name)
+# # num1.sort()
+# name.sort()
+# # num.reverse()
+
+# # num.pop(3)
+
+# print(name)
+# num = [1, 4, 5, 7,]
+# # num.insert(0, 23)
+# print(num)
+ 
+# names = ('ilyaz', 'asan', 'aygerim')
+# print(names{2, 5,))
+# number = (1, [1,2,3], "house", 3,5)
+# new = list(number)
+# # new[0] = 2
+# # number = tuple(number)
+# print(new)
+
+# number = [(23,3), (3,1,), (1,4), (3,6), (9,7)]
+
+# a = (1, 2, 3) 
+# print(a[0])
+# print(a[1])
+# print(a[2])
+
+# names = ['Ilyaz', 3.5,  1j, 5, [3,7,5,2], ("one", "two") ]
+
+# a = ["Tajibaev"]
+# b = ["Ilyaz"]
+# print(a + b)
+
+# name = ["Ilyaz"]
+# lastname = ["Tajibaev"]
+# name.extend(lastname)
+# print(name)
+
+# names = ["asan", "aliya", "ilyaz"]
+# b = ''
+# a = b.join(names)
+# print(a)
+
+# names = ["asan", "aliya", "ilyaz"]
+# # print(names.count("aliya"))
+# names.remove("asan")
+# names.pop(1)
+# print(names)
+
+# a = []
+# a.append("Ilyaz")
+# a.append("23.03.2001")
+# a.append("Python")
+# print(a)
+
+# names = ["asan", "aliya", "ilyaz"]
+# for i in names:
+#     print(i)
+# a = [1,1,2]
+# for i in a:
+#     print(i + 5)
+
+# chatnyi = []
+# nechatnyi = []
+# for i in range(21):
+#     if i % 2 == 0:
+#         chatnyi.append(i)
+#     else:
+#         nechatnyi.append(i)
+    
+# print(chatnyi)
+# print(nechatnyi)
+
+
+# string1 = "a2b3c4d5f6"
+# numbers = []
+# litters = []
+# for i in string1:
+#     if i.isdigit():
+#         numbers.append(int(i))
+#     else:
+#         litters.append(i)
+# print("Список чисел",numbers)
+# print("Список букв",litters)
+
+# user = "Ilyaz"
+# password = 23032001
+# u = input("Ведите имя пользователя - ")
+# p = int(input("Ведите пароль"))
+# if u == user and p == password:
+#     print("Добро пожаловать!")
+# else:
+#     print("Неверный логин или пароль")
+
+# sset = {1, 2, 3, 4, 5, 6, 7, 8}
+# sset.add(11)
+# sset.add(10)
+# sset.remove(1)
+# sset.discard(10)
+# sset.clear()
+# print(sset)
+
+# sset = {1, 2, 3, 4, 6}
+# sset1 = {1, 2, 3, 4, 7, 8, 9}
+# # result = sset.difference(sset1)
+# # result = sset.intersection(sset1)
+# # result = sset.intersection_update(sset1)
+# sset.remove(4)
+# sset1.remove(9)
+# print(sset)
+# print(sset1)
+
+# a = {"apple", "banana", "mango", "avacado"}
+# b = {"grape", "apple", "avacado", "peach"}
+# result = a.intersection(b)
+# print(result)
+
+# a = {"apple", "banana", "mango", "avacado", "peach"}
+# a.add("grape")
+# b = a.pop()
+# print(a)
+# print(b)
+# info = {"name": "Ilyaz", 'age': 23, 'fruit': ['apple', 'banano']}
+# print(info.values())
+# print(info.keys())
+# print(info.items())
+# print(info.get("name"))
+
+# a  = {'apple': 60, 'banana': 20}
+# b = {'apple': 40, 'mango': 61}
+# a.update(b)
+# print(a)
+
+# menu = {}
+# menu['plov'] = 130
+# menu['lagman'] = 135
+# menu['plov'] = 135
+# del menu['lagman']
+# menu['drings'] = ['Fanta', 'pepsi']
+# print(menu)
+
+# info = {}
+# for i in range(3):
+#     name = input('Ведите имя - ')
+#     password = int(input('Ведите пароль - '))
+#     info[name] = password
+# print(info)
+
+# sset = set{dir(set)}
+
+# info = {'Devid': 'Programer', 'Jack': 'Doctor'}
+# for k, v in info.items():
+#     print(f"Здраствуйте -  {k}, Прекрасная професия- {v}")
+
+# a = ['Kyrgystan', 'Kazakstan', 'Uzbekstan', 'Tajikstan', 'Uzbekstan']
+# b = set(a)
+# a = list(b)
+# print(a)
+
+# sset = []
+# for i in range(10):
+#     a = int(input())
+#     sset.append(a)
+# res = tuple(sset)    
+# print(res)
+
+# a = {"asan", "aygerim", "artyr", "ilyaz"}
+# b = {"almanbet", "artyr", "aygerim", "aman"}
+# result = a.difference(b)
+# print(result)
+
+# a = {"asan", "aygerim", "artyr", "ilyaz"}
+# b = {"almanbet", "artyr", "aygerim", "aman"}
+# result = a.intersection(b)
+# print(result)
+
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(sum(num))
+# print(max(num))
+# print(min(num))
+# print(sum(num) / len(num))
+# print(len(num))
+
+# for i in range(1, 11):
+#     print(i)
+# a = 17
+# b = 30
+# c = 4
+# while b > c:
+#     print(b, ">", c)
+
+# string = "Htllo"
+# # print(len(string))
+# i = 0
+# while i < len(string):
+#     print(string[i])
+#     i += 1
+
+# count = 0
+# count1 = 0
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         count += 1 
+#     else:
+#         count1 += 1
+# print(count)
+# print(count1)
+
+# my_list = [1, 2]
+# for i in my_list:
+#     print(i)
+# i = 0
+# while i < len(my_list):
+#      print(my_list[i])
+#      i += 1
+# password = 12345
+# count = 3
+# loged_in = False
+# while count > 0:
+#     user_input = int(input("Ведите пароль"))
+#     if user_input == password:
+#         loged_in = True
+#         break
+#     else:
+#         count -= 1
+#         print("Неверный пароль.Повторите попытку")
+# if loged_in:
+#     print("Вход выполнен")
+# else:
+#     print("У вас не осталось попыток")
+
+# list = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+# for i in list:
+#     if i == 25:
+#         break
+#     print(i)
+
+# sum = 0
+# num = int(input('Ведите число :'))
+# while  num != 0:
+#     sum += num
+#     num = int(input('Ведите число :'))
+# print('Сумма веденых чисел - ', sum)
+
+# list = [2, 3, 4, 6, 7]
+# for i in list:
+#     print(f'Строка {i}-0')
+
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+# print(sum)
+
+# a = 35
+# b = 23
+# c = 35
+# if a == b or a == c or b == c:
+#     print("Yes")
+# else:
+#     print("Error")
+
+# list1 = [3, 5, 2, 1, 4]
+# list2 = [4, 2, 6, 8, 1]
+# a = list1 + list2
+# b = set(a)
+# a = list(b)
+
+# print(a)
+
+# num = []
+# for i in range(1, 16):
+#     num.append(i)
+# print(min(num) + max(num))      
+
+# a = int(input("Ведите число месяца -"))
+# if a ==1 or a == 2 or a == 12:
+#     print("Winter")
+# elif a >= 3 and a <= 5:
+#     print("Spring")
+# elif a >= 6 and a <= 8:
+#     print("Summer")
+# elif a >= 9 and a <= 11:
+#     print("Autumn")
+# else:
+#     print("Некорректный число месяца -")
+
+# r = int(input("Ведите радиус "))
+# res = 2 * 3.14 * r
+# print(f"Длина окружности - {res}")
