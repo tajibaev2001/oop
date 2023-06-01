253defaa296491ded3456f0f18506966d074f53e
tajibaev2001 <tajibaev200@gmail.com>
2023-06-01 20:04:18 +0600

    module

---
+++ mission.py
@@ -0,0 +1,41 @@
+# def info(name, salary=5000):
+#      return f"{name} - {salary}"
+
+# name = "Asan"
+# salary = 10000
+# result = info(name, salary)
+# print(result)
+
+# def generate_list():
+#     num = int(input())
+#     my_list  = []
+#     for i in range(1, num+1):
+#         my_list.append(i)
+#     return my_list
+
+# print(generate_list())
+
+# lst = [4, 7, 2, 9, 9, 2, 4, 5, 6]
+# list1 = set(lst)
+# list1.intersection()
+# list2 = list(list1)
+# print(list2)
+
+# numbers = [1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789]
+# for i in range (len(numbers) - 1):
+#     a = numbers[i]
+#     b = numbers[i+1]
+#     sum = a + b
+#     print(sum)
+
+# from datetime import datetime
+
+# current_time = datetime.today()
+# print(current_time.time())
+
+# import time
+
+# for i in range(10):
+#     time.sleep(5)
+#     print(i)
+
