1b90d954a726290a4bb64d2e8d2d80ed21cb7abb
tajibaev2001 <tajibaev200@gmail.com>
2023-06-02 20:09:03 +0600

    module

--- mission.py
+++ mission.py
@@ -39,3 +39,111 @@
 #     time.sleep(5)
 #     print(i)
 
+# names = ['Jyldyz', 'Aigerim', 'Bekmamat', 'Nurbek', 'Azamat','Shirin']
+
+
+# print(names[2:5])
+
+# list = ['Ilyaz', 'Asan', 'Alya', 'Aygerim']
+# list2 = ""
+# a = list2.join(list)
+# print(a)
+
+# set = {5, 6, 7, 8, 3, 9}
+# set.add(1)
+# set.pop()
+# print(set)
+
+# for i in range(1,10):
+
+#     # if i == 5:
+
+#         print(i)
+
+#     # else:
+
+    #     print("Unkown Number")
+
+
+
+
+# import os 
+# import shutil
+
+# def register():
+#     login = input("Введите логин: ")
+#     password = input("Введите пароль: ")
+#     photo_path = input("Введитие путь до фото: ")
+    
+#     if os.path.exists(photo_path):
+#         _,photo_split = os.path.splitext(photo_path)
+#         valid_photo_split = ['.jpeg', '.jpg', '.png']
+        
+#         if photo_split.lower() in valid_photo_split:
+#             photo = f"registred_foto/{login}{photo_split}"   
+            
+#             shutil.copyfile(photo_path, photo)
+            
+#             with open("database.txt", 'a') as file:
+#                 file.write(f"{login}:{password}:{photo}\n")
+#             print("Регистрация успешна завершена")
+        
+#         else:
+#             print("Неподдерживаемое расширение фотографии. Регистрация отменена")
+#     else:
+#         print("Фото не найдено. Регистрация отменена")        
+        
+# os.makedirs('registred_foto', exist_ok=True)
+
+# register()
+
+# def aritfmetic():
+#     num1 = int(input())
+#     num3 = input()
+#     num2 = int(input())
+#     if num3 == "+":
+#         return num1 + num2
+#     elif num3 == "-":
+#         return num1 - num2
+#     elif num3 == "*":
+#         return num1 * num2
+#     elif num3 == "/":
+#         return num1 / num2
+#     else:
+#         print("Неизвестная операция")
+# print(aritfmetic())
+
+# r = int(input("Введите цифру "))
+# res1 = r * 4
+# res3 = r**2 + r**2
+# res4 = r**2
+# print(f"Периметр- {res1}")
+# print(f"Диоганаль {res3}")
+# print(f"Квадрат {res3}")
+
+# import math
+# def square(a):
+#     p = a * 4
+#     s = a ** 2
+#     d = (a ** 2) + (a ** 2)
+#     d2 = math.sqrt(d)
+#     print(f"Переметр - {p}, Плошадь - {s}, Диоганаль - {round(d2, 1)}")
+
+# a = int(input())
+# square(a)
+
+# import os
+# import shutil
+
+# def image():
+#     image1 = input()
+#     image2 = input()
+#     if os.path.exists(image1) and os.path.exists(image2):
+#         shutil.copyfile(image1, image2)
+#     else:
+#         if not os.path.exists(image1):
+#             print(f"Файл {image1} отсуствует")
+#         if not os.path.exists(image2):
+#             print(f"Файл {image2} отсуствует")
+
+# image()
